9da33e04d1f9c693661ffeec78fe18eba79c7d18
lililin629 <lilian.lintzuyu@gmail.com>
2024-04-27 09:39:26 -0400

    imported NullWritable

--- top10_movie_type/MovieRevenueDriver.java
+++ top10_movie_type/MovieRevenueDriver.java
@@ -5,6 +5,7 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.NullWritable;
 
 public class MovieRevenueDriver {
     public static void main(String[] args) throws Exception {
