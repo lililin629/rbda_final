d423e3ae0c7b6220a430c994e1c4a973f0295a7c
lililin629 <lilian.lintzuyu@gmail.com>
2024-04-29 16:48:08 -0400

    dl parenthesis

--- clean/CSVMapper.java
+++ clean/CSVMapper.java
@@ -10,6 +10,47 @@ import java.io.IOException;
 import java.util.Scanner;
 import org.apache.hadoop.io.NullWritable;
 
+// public class CSVMapper extends Mapper<Object, Text, NullWritable, Text> {
+
+//     private Text outputValue = new Text();
+
+//     public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+//         // Use Scanner to read the line using a comma delimiter while respecting quotes
+//         Scanner scanner = new Scanner(value.toString());
+//         scanner.useDelimiter(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
+
+//         try {
+//             if (scanner.hasNext()) {
+//                 String movieName = scanner.next().trim();
+//                 String movieDate = scanner.next().trim();
+//                 scanner.next(); // Skip 'Serie Name'
+//                 scanner.next(); // Skip 'Serie Date'
+//                 String movieType = scanner.next().trim();
+//                 scanner.next(); // Skip 'Number of Votes'
+//                 String movieRevenue = scanner.next().trim();
+//                 String score = scanner.next().trim();
+//                 String metascore = scanner.next().trim();
+//                 scanner.next(); // Skip 'Time Duration'
+//                 String director = scanner.next().trim();
+
+//                 // Construct the output value without the movie name as key
+//                 String output = movieName + "," + movieDate + "," + movieType + "," + movieRevenue + "," + score + ","
+//                         + metascore + "," + director;
+//                 outputValue.set(output);
+
+//                 // Emit with null key
+//                 context.write(NullWritable.get(), outputValue);
+//             }
+//         } finally {
+//             scanner.close();
+//         }
+//     }
+// }
+
+import org.apache.hadoop.io.NullWritable;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class CSVMapper extends Mapper<Object, Text, NullWritable, Text> {
 
     private Text outputValue = new Text();
@@ -32,6 +73,9 @@ public class CSVMapper extends Mapper<Object, Text, NullWritable, Text> {
                 String metascore = scanner.next().trim();
                 scanner.next(); // Skip 'Time Duration'
                 String director = scanner.next().trim();
+
+                // Removing parentheses from movieDate
+                movieDate = movieDate.replaceAll("[()]", ""); // Regex to remove any parentheses
 
                 // Construct the output value without the movie name as key
                 String output = movieName + "," + movieDate + "," + movieType + "," + movieRevenue + "," + score + ","
